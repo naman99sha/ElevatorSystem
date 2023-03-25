e65abb066ad1e6079526243b0f8d4d8800deed40
naman99sha <naman99sha@gmail.com>
2023-03-25 17:20:48 +0530

    Modified settings.py to include new apps and connect to postgresql

--- ElevatorSystemProject/settings.py
+++ ElevatorSystemProject/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'admin',
+    'elevator',
+    'floor'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +77,21 @@ WSGI_APPLICATION = 'ElevatorSystemProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'elevatordb',
+        'USER': 'postgres',
+        'PASSWORD': '<Enter your postgresql password>',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
